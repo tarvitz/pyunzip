a1e4277759c621a32185f38f434c54b0e182aed6
Tarvitz <lilfoxster@gmail.com>
2011-03-09 12:13:50 +1100

    farseer common implementation

---
+++ apps/farseer/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+# Create your models here.
+
+class Discount(models.Model):
+    name = models.CharField(_('Game name'),max_length=100)
+    price = models.FloatField(_('Price'))
+    current_price = models.FloatField(_('Discount'))
+
+    def __unicode__(self):
+        return name
+    
+    @property
+    def discount(self):
+        return int(
+            100-round(self.current_price/(self.price/100))
+        )
+
