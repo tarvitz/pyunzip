fc3e1186b6c9d127817db7adf3115b124e3df0a6
Tarvitz <lilfoxster@gmail.com>
2011-05-03 18:17:49 +1200

    fix js tinymk library

--- media/js/tinymk.js
+++ media/js/tinymk.js
@@ -18,15 +18,15 @@ function change_syntax(obj){
 		syntax_object = document.getElementById('id_hidden_syntax');
 	    syntax_main = document.getElementById('id_syntax');
 		if (syntax_main){
-			if (syntax_object.value == 'bb-code') syntax_main.value = 'markdown';
+			if (syntax_object.value == 'bb-code') syntax_main.value = 'textile';
 			else syntax_main.value = 'bb-code';
 		 }//if syntax_main
 
 		//alert(syntax_object.value);
-		if (syntax_object.value == '') syntax_object.value = 'markdown';
+		if (syntax_object.value == '') syntax_object.value = 'textile';
 
 		if (syntax_object.value == 'bb-code'){
-			syntax_object.value = 'markdown';
+			syntax_object.value = 'textile';
 			img_obj.src = '/media/tinymk/markdown.png';
 			img_obj.title = 'Markdown syntax is active';
 		}
@@ -84,12 +84,12 @@ function paste_code(obj,block){
 			};
 			case 'italic':{
 				
-				insertBlockTag(block, {start: "*", end: "*"});
+				insertBlockTag(block, {start: "__", end: "__"});
                                 //textarea_object.value += '* *';
 				break;
 			};
 			case 'underline':{
-				insertBlockTag(block, {start: "_", end: "_"});
+				insertBlockTag(block, {start: "+", end: "+"});
                                 //textarea_object.value += '_ _'
 				break;
 			};
