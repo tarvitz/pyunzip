b9b07f07d3835f652180d18ed85cbcdf26a5662b
Tarvitz <tarvitz@blacklibrary.ru>
2011-09-04 16:18:25 +0400

    Edited settings_net.py via GitHub

--- settings_net.py
+++ settings_net.py
@@ -1,6 +1,6 @@
 #coding: utf-8
-EMAIL_HOST='smtp.inbox.ru'
-EMAIL_HOST_USER='lilfox'
-EMAIL_HOST_PASSWORD='x44m52f'
-FROM_EMAIL='AstroPath (no replay) <lilfox@inbox.ru>'
+EMAIL_HOST=''
+EMAIL_HOST_USER=''
+EMAIL_HOST_PASSWORD=''
+FROM_EMAIL='AstroPath (no replay) <user@host>'
 #
