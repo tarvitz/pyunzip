7c7d1b469ab12c9d2b8d4c061a2e96850ef26f56
lilfox <lilfoxster@gmail.com>
2013-02-04 09:09:08 +0400

    set default locale paths

--- settings/dist.py
+++ settings/dist.py
@@ -32,6 +32,9 @@ LANGUAGES = (
     ('ru', _('Russian')),
     ('en', _('English')),
 )
+LOCALE_PATHS = (
+    rel_path('conf/locale')
+)
 # If you set this to False, Django will make some optimizations so as not
 # to load the internationalization machinery.
 USE_I18N = True
