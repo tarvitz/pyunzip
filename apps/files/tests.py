cd44138fc795d59f391ac25564ca1f679c752141
lilfox <lilfoxster@gmail.com>
2013-04-01 05:24:47 +0400

    start tests for files within UserFile tests

---
+++ apps/files/tests.py
@@ -0,0 +1,160 @@
+# coding: utf-8
+#from django.utils import unittest
+import os
+from django.test import TestCase
+from apps.files.models import UserFile
+from apps.core.helpers import model_json_encoder
+from django.test.client import RequestFactory
+from django.core.urlresolvers import reverse
+from django.conf import settings
+import simplejson as json
+
+from apps.core.helpers import get_object_or_None
+
+
+class JustTest(TestCase):
+    fixtures = [
+        'tests/fixtures/load_users.json',
+    ]
+
+    def setUp(self):
+        self.request_factory = RequestFactory()
+        self.urls_void = [
+            "/news/",
+            '/news/archived/',
+            '/markup/preview/',
+            '/article/created/'
+        ]
+        self.urls_registered = [
+            '/article/add/',  # 302
+            "/news/user/",  # 302
+        ]
+        self.urls_302 = [
+
+        ]
+        self.urls_params = [
+            reverse('news:news', args=('testing',)),
+            reverse('news:article', args=(1,)),
+        ]
+        self.unlink_files = []
+
+    def tearDown(self):
+        for unlink in self.unlink_files:
+            try:
+                os.unlink(unlink)
+            except OSError:
+                pass
+
+    def print_form_errors(self, form):
+        if 'errors' in form:
+            for (key, items) in form['errors'].items():
+                print "%(key)s: %(items)s" % {
+                    'key': key,
+                    'items': ", ".join(items)
+                }
+
+    def no_test_urls_params(self):
+        messages = []
+        for user in ('user', 'admin', None):
+            if user:
+                self.client.login(username=user, password='123456')
+            else:
+                self.client.logout()
+            for url in self.urls_params:
+                response = self.client.get(url)
+                try:
+                    self.assertEqual(response.status_code, 200)
+                except AssertionError as err:
+                    messages.append({'url': url, 'user': user, 'err': err})
+        if messages:
+            for msg in messages:
+                print "Got error in (%s): %s, with %s" % (
+                    msg['user'], msg['url'], msg['err']
+                )
+            raise AssertionError
+
+    def test_jquery_file_upload(self):
+        the_file = open('tests/fixtures/avatar.png')
+        post = {
+            'title': 'avatar',
+            'file': the_file
+        }
+        # anonymous is not allow to upload
+        url = reverse('files:file-upload')
+        count = UserFile.objects.count()
+        response = self.client.post(url, post, follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(count, UserFile.objects.count())
+
+        # user can upload files normally
+        the_file.seek(0)
+        post.update({'file': the_file})
+        logged = self.client.login(username='user', password='123456')
+        self.assertEqual(logged, True)
+        response = self.client.post(url, post, follow=True)
+        self.assertEqual(response.get('Content-Type'), 'application/json')
+        js = json.loads(response.content)
+        if 'form' in js:
+            self.print_form_errors(js['form'])
+
+        self.assertEqual(response.status_code, 200)
+
+        self.assertEqual(count + 1, UserFile.objects.count())
+        user_file = UserFile.objects.all()[0]
+        path = os.path.join(settings.MEDIA_ROOT, user_file.file.path)
+        os.lstat(path)
+        self.assertEqual(user_file.plain_type, 'image/png')
+        self.assertEqual(user_file.size, user_file.file.size)
+        self.unlink_files.append(user_file.file.path)
+
+    def test_delete_file(self):
+        self.test_jquery_file_upload()
+        self.client.login(username='user', password='123456')
+        count = UserFile.objects.filter(owner__username='user').count()
+        user_file = UserFile.objects.filter(owner__username='user')[0]
+        url = user_file.get_delete_url()
+        response = self.client.get(url, follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            count - 1,
+            UserFile.objects.filter(owner__username='user').count()
+        )
+        self.assertEqual(os.path.exists(user_file.file.path), False)
+
+    def test_serialize_usefile(self):
+        self.client.login(username='user', password='123456')
+        post = {
+            'title': 'avatar',
+            'file': open('tests/fixtures/avatar.png')
+        }
+        url = reverse('files:file-upload')
+        count = UserFile.objects.filter(owner__username='user').count()
+        response = self.client.post(url, post, follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.get('Content-Type'), 'application/json')
+        js = json.loads(response.content)
+        if 'form' in js:
+            self.print_form_errors(js['form'])
+        new_count = UserFile.objects.filter(owner__username='user').count()
+
+        print "Got file instance: %s" % js['file']
+        self.assertIn('avatar.png', js['file']['url'])
+        self.assertEqual(count + 1, new_count)
+        user_file = UserFile.objects.filter(owner__username='user')[0]
+        js = json.dumps(user_file, default=model_json_encoder)
+        self.assertIn(user_file.file.name, js)
+        self.unlink_files.append(user_file.file.path)
+
+    def test_big_file_uploading(self):
+        self.client.login(username='user', password='123456')
+        post = {
+            'title': 'big file',
+            'file': open('tests/fixtures/big_file.zip')
+        }
+        url = reverse('files:file-upload')
+        response = self.client.post(url, post, follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.get('Content-Type'), 'application/json')
+        js = json.loads(response.content)
+        self.assertIn('form', js)
+        self.assertIn('errors', js['form'])
