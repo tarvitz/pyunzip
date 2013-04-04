8137e923ff002a911f4df1fc622d1fe5b67fe196
lilfox <lilfoxster@gmail.com>
2013-04-04 06:12:04 +0400

    update jsonapp url

--- apps/jsonapp/urls.py
+++ apps/jsonapp/urls.py
@@ -12,10 +12,10 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    url(r'^', include('apps.wh.urls_json', namespace='wh')),
+    url(r'^', include('apps.wh.urls.json', namespace='wh')),
     #url(r'^', include('apps.core.urls_json', namespace='core')),
     #url(r'^', include('apps.news.urls_json', namespace='news')),
-    url(r'^', include('apps.files.urls_json', namespace='files')),
-    url(r'^', include('apps.tabletop.urls_json', namespace='tabletop')),
-    url(r'^', include('apps.tracker.urls_json', namespace='tracker')),
+    url(r'^', include('apps.files.urls.json', namespace='files')),
+    url(r'^', include('apps.tabletop.urls.json', namespace='tabletop')),
+    url(r'^', include('apps.tracker.urls.json', namespace='tracker')),
 )
