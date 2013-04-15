e50db4699285701c5baa16a50b34b8f6cde0271f
lilfox <lilfoxster@gmail.com>
2013-04-15 16:52:06 +0400

    update render filter for get head content report

--- apps/tabletop/models.py
+++ apps/tabletop/models.py
@@ -244,11 +244,11 @@ class BattleReport(models.Model):
 
     def get_head_content(self):
         if '(cut)' in self.comment:
-            return render_filter(
-                post_markup_filter(
+            return  post_markup_filter(
+                render_filter(
                     self.comment[:self.comment.index('(cut)')]
-                ),
-                self.syntax or 'textile'
+                    self.syntax or 'textile'
+                )
             )
         return render_filter(self.comment, self.syntax or 'textile')
 
