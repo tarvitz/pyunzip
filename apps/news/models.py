4227e71fc4256294d9ee40b02d5f6a94edc28863
lilfox <lilfoxster@gmail.com>
2013-04-24 04:22:40 +0400

    update news models pep8 check

--- apps/news/models.py
+++ apps/news/models.py
@@ -18,6 +18,7 @@ NEWS_STATUSES = (
     ('revision', _('revision')),
 )
 
+
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(_('Category'), max_length=100, blank=False, null=False)
@@ -26,49 +27,60 @@ class Category(models.Model):
         return self.name
 
     class Meta:
-        verbose_name=_('Category')
-        verbose_name_plural=_('Categories')
+        verbose_name = _('Category')
+        verbose_name_plural = _('Categories')
+
 
 class AbstractSubstance(models.Model):
-    title = models.CharField(_('Title'), max_length=255,null=False)
-    author = models.CharField(_('Author'), max_length=40)
-    content = models.CharField(_('Content'), max_length=10240)
-    author_ip = models.IPAddressField(_('IP address',blank=True))
-    syntax = models.CharField(_('Syntax'), max_length=20,blank=True,choices=settings.SYNTAX)
+    title = models.CharField(
+        _('title'), max_length=255, null=False)
+    author = models.CharField(_('author'), max_length=40)
+    content = models.CharField(_('content'), max_length=10240)
+    author_ip = models.IPAddressField(_('ip address', blank=True))
+    syntax = models.CharField(
+        _('syntax'), max_length=20,
+        blank=True,
+        choices=settings.SYNTAX
+    )
 
     class Meta:
         abstract = True
 
+
+# noinspection PyUnresolvedReferences,PyUnresolvedReferences
 class AbstractNews(models.Model):
     title = models.CharField(
-        _('Title'), max_length=255,null=False
+        _('title'), max_length=255,
+        null=False
     )
     author = models.CharField(
-        _('Author'), max_length=30,
+        _('author'), max_length=30,
         null=False
     )
     editor = models.CharField(
-        _('Editor'), max_length=30, blank=True
+        _('editor'), max_length=30, blank=True
     )
     url = models.CharField(
-        _('Original URL'), max_length=200, blank=True
+        _('original URL'), max_length=200, blank=True
     )
-    # replace with cut
-    #head_content = models.TextField(
-    #    _('Head content'), blank=True, null=True
-    #)
-    content =  models.TextField(_('Content'), null=False)
-    date = models.DateTimeField(_('DateTime'), null=False, default=datetime.now)
-    approved = models.BooleanField(_('Approved'), blank=True)
-    author_ip = models.CharField(_('Author IP address'), max_length=16, blank=True)
+    content = models.TextField(
+        _('content'), null=False)
+    date = models.DateTimeField(
+        _('dateTime'), null=False, default=datetime.now)
+    approved = models.BooleanField(
+        _('approved'), blank=True)
+    author_ip = models.CharField(
+        _('author ip address'), max_length=16, blank=True)
     category = models.ForeignKey(Category)
-    is_event = models.BooleanField(_('Is event'))
+    is_event = models.BooleanField(_('is event'))
     syntax = models.CharField(
-        _('Syntax'),max_length=20, blank=True, null=True, choices=settings.SYNTAX) #markdown is default
+        _('Syntax'), max_length=20, blank=True, null=True,
+        choices=settings.SYNTAX
+    )
     attachment = models.ForeignKey(Attachment, blank=True, null=True)
-
     reason = models.CharField(_('reason'), max_length=1024, blank=True, null=True)
-    owner = models.ForeignKey('auth.User', verbose_name='owner',
+    owner = models.ForeignKey(
+        'auth.User', verbose_name='owner',
         related_name='%(class)s', default=1
     )
     status = models.CharField(
@@ -77,14 +89,14 @@ class AbstractNews(models.Model):
         max_length=32,
     )
     resend = models.BooleanField(
-        _('resend'), help_text=_("marks if news notification should resend"),
+        _('resend'),
+        help_text=_("marks if news notification should resend"),
         default=False
     )
     seen_objects = generic.GenericRelation(
         'tracker.SeenObject', object_id_field='object_pk'
     )
 
-    #wise alias ;) onto head_content
     get_title = lambda self: self.title
     get_content = lambda self: self.content
 
@@ -117,7 +129,7 @@ class AbstractNews(models.Model):
     def head_content(self):
         return self.get_head()
 
-    def get_status_label(self, framework='bootstrap'):
+    def get_status_label(self):
         if self.status == 'rejected':
             return 'important'
         elif self.status == 'queued':
@@ -141,15 +153,19 @@ class AbstractNews(models.Model):
 
 
 class ArchivedNews(AbstractNews):
-    search = SphinxSearch(weights={
-        'title': 100,
-        'head_content': 100,
-        'content': 100,
-    })
+    search = SphinxSearch(
+        weights={
+            'title': 100,
+            'head_content': 100,
+            'content': 100,
+        }
+    )
+
     class Meta:
         verbose_name = _('Archived Article')
         verbose_name_plural = _('Archived News')
         ordering = ['-id']
+
     #def get_absolute_url(self):
     #    return reverse('apps.news.views.show_archived_article', kwargs={'number': self.id})
 
@@ -158,35 +174,40 @@ class ArchivedNews(AbstractNews):
     #    copy_fields(self,n)
     #    n.save()
     #    self.delete()
+
     def get_absolute_url(self):
-        return reverse('news:article-archived',kwargs={'id':self.id})
+        return reverse('news:article-archived', kwargs={'id': self.id})
 
     class Meta:
         permissions = (
-            ('edit_archived_news','Can edit archived news'),
+            ('edit_archived_news', _('Can edit archived news')),
         )
         verbose_name = _('Archived news')
         verbose_name_plural = _('Archived news')
 
-class News(AbstractNews):
-    search = SphinxSearch(weights={
-        'title': 100,
-        'head_content': 100,
-        'content': 100,
-    })
 
+class News(AbstractNews):
+    search = SphinxSearch(
+        weights={
+            'title': 100,
+            'head_content': 100,
+            'content': 100,
+        }
+    )
 
     def get_absolute_url(self):
-        return reverse('news:article',kwargs={'number':self.id})
+        return reverse('news:article', kwargs={'number': self.id})
 
     def archive(self):
         a = ArchivedNews()
-        copy_fields(self,a)
+        copy_fields(self, a)
         #actions with comments
         a.save()
-        old_ct = ContentType.objects.get(app_label='news',model='news')
-        new_ct = ContentType.objects.get(app_label='news',model='archivednews')
-        comments = Comment.objects.filter(content_type=old_ct,object_pk=str(self.id))
+        old_ct = ContentType.objects.get(app_label='news', model='news')
+        new_ct = ContentType.objects.get(app_label='news', model='archivednews')
+        comments = Comment.objects.filter(
+            content_type=old_ct, object_pk=str(self.id)
+        )
         for c in comments:
             c.content_type = new_ct
             c.object_pk = str(a.id)
@@ -197,46 +218,65 @@ class News(AbstractNews):
 
     def get_content_plain(self):
         if self.head_content:
-            return "%s\n%s\n%s" %(self.title,self.head_content,self.content)
+            return "%s\n%s\n%s" % (
+                self.title, self.head_content, self.content
+            )
         else:
-            return "%s\%s" % (self.title,self.content)
+            return "%s\%s" % (self.title, self.content)
 
     class Meta:
         verbose_name = _('Article')
         verbose_name_plural = _('News')
         permissions = (
-            ('edit_news', 'Can edit news'),
-            ('del_restore_comments', 'Can delete and restore comments'),
-            ('edit_comments', 'Can edit comments'),
-            ('purge_comments', 'Can purge comments'),
+            ('edit_news', _('Can edit news')),
+            ('del_restore_comments', _('Can delete and restore comments')),
+            ('edit_comments', _('Can edit comments')),
+            ('purge_comments', _('Can purge comments')),
         )
 
+
 class MeatingType(models.Model):
-    title = models.CharField(_("Title"), max_length=64)
-    codename = models.CharField(_("Codename"), unique=True, max_length=32)
+    title = models.CharField(_("title"), max_length=64)
+    codename = models.CharField(_("codename"), unique=True, max_length=32)
     __unicode__ = lambda s: "%s [%s]" % (s.title, s.codename)
 
     class Meta:
         verbose_name = _("Meating Type")
         verbose_name_plural = _("Meating Types")
 
+
+# noinspection PyShadowingBuiltins
 class Meating(models.Model):
-    title = models.CharField(_("Title"), max_length=256)
-    type = models.ForeignKey(MeatingType, verbose_name=_("Type"), default=1)
-    owner = models.ForeignKey(User, verbose_name=_("Owner"),
+    title = models.CharField(_("title"), max_length=256)
+    type = models.ForeignKey(MeatingType, verbose_name=_("type"), default=1)
+    owner = models.ForeignKey(
+        User, verbose_name=_("owner"),
         related_name='meating_owner_set')
-    members = models.ManyToManyField(User, verbose_name=_("Members"),
-        related_name='meating_user_sets')
-    content = models.TextField(_("Content"), max_length=4096)
-    syntax = models.CharField(_('Syntax'),max_length=20, blank=True,
-        null=True, choices=settings.SYNTAX, default='bb-code') #markdown is default
-    created_on = models.DateTimeField(_("Created on"), auto_now_add=True)
-    updated_on = models.DateTimeField(_("Updated on"), auto_now=True)
-    is_approved = models.BooleanField(_('Approved'), default=False)
-    author_ipv4 = models.CharField(_('Author IP address'),
-        max_length=16, blank=True)
-    author_ipv6 = models.CharField(_('Author IP address'),
-        max_length=16, blank=True)
+    members = models.ManyToManyField(
+        User, verbose_name=_("members"),
+        related_name='meating_user_sets'
+    )
+    content = models.TextField(_("content"), max_length=4096)
+    syntax = models.CharField(
+        _('syntax'), max_length=20, blank=True,
+        null=True, choices=settings.SYNTAX,
+        default='textile'
+    )
+    created_on = models.DateTimeField(
+        _("created on"), auto_now_add=True
+    )
+    updated_on = models.DateTimeField(
+        _("updated on"), auto_now=True
+    )
+    is_approved = models.BooleanField(_('approved'), default=False)
+    author_ipv4 = models.CharField(
+        _('author ip address'),
+        max_length=16, blank=True
+    )
+    author_ipv6 = models.CharField(
+        _('author ip address'),
+        max_length=16, blank=True
+    )
 
     def get_absolute_url(self):
         return reverse('news:meating', args=(self.id, ))
@@ -247,7 +287,7 @@ class Meating(models.Model):
     class Meta:
         verbose_name = _("Meating")
         verbose_name_plural = _("Meatings")
-        ordering = ['created_on', '-id',]
+        ordering = ['created_on', '-id', ]
 
 from signals import setup_signals
 setup_signals()
