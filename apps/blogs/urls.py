ec0d9b1dfe88e381d9ac1e275946aecf325b4b76
Tarvitz <tarvitz@blacklibrary.ru>
2013-04-26 13:39:41 -0500

    update code insert class

--- apps/blogs/urls.py
+++ apps/blogs/urls.py
@@ -3,9 +3,9 @@ from django.conf import settings
 from django.utils.translation import ugettext_lazy as _
 
 urlpatterns = patterns('apps.blogs.views',
-    url(r'^$','blog_index', name='index'),
-    url(r'^post/(?P<id>\d+)/$','show_post',{'object_model': 'blogs.post'}, name='post'),
-    url(r'^by/tag/(?P<tag_id>\d+)/$', 'get_posts_via_tag', name='tag'),
+    #url(r'^$','blog_index', name='index'),
+    #url(r'^post/(?P<id>\d+)/$','show_post',{'object_model': 'blogs.post'}, name='post'),
+    #url(r'^by/tag/(?P<tag_id>\d+)/$', 'get_posts_via_tag', name='tag'),
     #(r'karma/(?P<user>[\w\s]+)/$', 'show_karma'),
 )
 
