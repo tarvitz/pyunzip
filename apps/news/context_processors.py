a4d9760ca84e79c6506478cdfd25640ae57c9526
Tarvitz <tarvitz@blacklibrary.ru>
2014-05-31 00:55:09 +0400

    update news_event for calendar noty

--- apps/news/context_processors.py
+++ apps/news/context_processors.py
@@ -6,8 +6,9 @@ from datetime import datetime, timedelta
 def weekly_events(request):
     now = datetime.now()
     #start = datetime(*now.timetuple()[:3]) - timedelta(days=now.weekday())
-    end = datetime(*now.timetuple()[:3]) + timedelta(
-        days=6 - now.weekday(), hours=23, minutes=59)
+    #end = datetime(*now.timetuple()[:3]) + timedelta(
+    #    days=6 - now.weekday(), hours=23, minutes=59)
+    end = now + timedelta(weeks=1)
     new_events = Event.objects.filter(is_finished=False, date_start__gte=now,
                                       date_end__lte=end)
     return {
