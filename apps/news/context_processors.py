d516476f58bef2ce6b14bacc0c64505878794be0
Tarvitz <tarvitz@blacklibrary.ru>
2014-05-31 04:57:28 +0400

    update new events counter

--- apps/news/context_processors.py
+++ apps/news/context_processors.py
@@ -9,7 +9,7 @@ def weekly_events(request):
     #end = datetime(*now.timetuple()[:3]) + timedelta(
     #    days=6 - now.weekday(), hours=23, minutes=59)
     end = now + timedelta(weeks=1)
-    new_events = Event.objects.filter(is_finished=False, date_start__gte=now,
+    new_events = Event.objects.filter(is_finished=False,
                                       date_end__lte=end)
     return {
         'new_events': new_events
