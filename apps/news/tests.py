8611b37b15162d287d31ab18c2f606cffc8117db
Tarvitz <tarvitz@blacklibrary.ru>
2014-06-02 19:30:29 +0400

    update event_watch tests

--- apps/news/tests.py
+++ apps/news/tests.py
@@ -4,7 +4,10 @@ from django.test import TestCase
 #import unittest
 #from django.test.client import RequestFactory, Client
 from apps.core.tests import TestHelperMixin
-from apps.news.models import Category, News, Event, EVENT_TYPE_CHOICES
+from apps.news.models import (
+    Category, News, Event, EventWatch,
+    EVENT_TYPE_CHOICES
+)
 try:
     from django.contrib.auth import get_user_model
     from django.contrib.auth.models import AnonymousUser
@@ -664,3 +667,54 @@ class EventTest(TestHelperMixin, TestCase):
         response = self.client.post(url, self.post, follow=True)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(Event.objects.count(), count + 1)
+
+
+class EventWatchTest(TestHelperMixin, TestCase):
+    fixtures = [
+        'tests/fixtures/load_users.json',
+        'tests/fixtures/load_event_places.json',
+        'tests/fixtures/load_events.json'
+    ]
+
+    def setUp(self):
+        # process events and reset date_start, date_end for
+        # further test runs corrections
+        now = datetime.now()
+        for event in Event.objects.all():
+            offset = now - event.date_start
+            event.date_start += timedelta(seconds=offset.seconds)
+            event.date_end += timedelta(seconds=offset.seconds)
+            event.save()
+
+    def test_event_watched(self):
+        """
+        common test for watched event by user watch event action,
+        eventwatch instance should be registered
+        """
+        self.login('user')
+        user = User.objects.get(username='user')
+        event = Event.objects.exclude(is_finished=True)[0]
+        count = EventWatch.objects.count()
+        self.assertEqual(count, 0)
+
+        response = self.client.get(event.get_absolute_url(), follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(EventWatch.objects.count(), count + 1)
+        event_watch = EventWatch.objects.latest('pk')
+        self.assertEqual(event_watch.user, user)
+        self.assertEqual(event_watch.event, event)
+
+    def test_event_watched_is_finished(self):
+        """
+        common test for wached event by user event action if event is finished,
+        eventwatch instance shouldn't be registered
+        """
+        self.login('user')
+        user = User.objects.get(username='user')
+        event = Event.objects.filter(is_finished=True)[0]
+        count = EventWatch.objects.count()
+        self.assertEqual(count, 0)
+
+        response = self.client.get(event.get_absolute_url(), follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(EventWatch.objects.count(), count)
