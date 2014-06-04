73f05fcc83926080b0832899a7409bd828735537
Tarvitz <tarvitz@blacklibrary.ru>
2014-06-04 08:03:58 +0400

    fix files

--- apps/files/models.py
+++ apps/files/models.py
@@ -4,9 +4,6 @@ from django.contrib.contenttypes import generic
 from django.conf import settings
 from django.db import models
 
-from django.contrib.auth import get_user_model
-User = get_user_model()
-
 from django.utils.translation import ugettext_lazy as _
 from django.core.urlresolvers import reverse
 from django.core.exceptions import ValidationError
