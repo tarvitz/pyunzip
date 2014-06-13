5bad6bbf5c59496ccffe80df5fe2044b6e133bb7
Tarvitz <tarvitz@blacklibrary.ru>
2014-06-13 21:21:28 +0400

    update user security for rest-api crud

--- apps/accounts/tests/rest.py
+++ apps/accounts/tests/rest.py
@@ -1,5 +1,14 @@
+# coding: utf-8
+
 from apps.accounts.models import User
+from apps.core.tests import TestHelperMixin
 from tastypie.test import ResourceTestCase
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from django.core.urlresolvers import reverse
+import simplejson as json
+from copy import deepcopy
 
 
 class UserResourceTest(ResourceTestCase):
@@ -9,3 +18,162 @@ class UserResourceTest(ResourceTestCase):
 
     def setUp(self):
         super(UserResourceTest, self).setUp()
+
+
+class UserViewSetTest(TestHelperMixin, APITestCase):
+    fixtures = [
+        'tests/fixtures/load_users.json',
+    ]
+
+    def setUp(self):
+        self.user = User.objects.get(username='user')
+        new_id = User.objects.count() + 1
+        self.url_detail = reverse('api:user-detail', args=(self.user.pk, ))
+        self.url_put = reverse('api:user-detail', args=(self.user.pk, ))
+        self.url_patch = reverse('api:user-detail', args=(self.user.pk, ))
+        self.url_delete = reverse('api:user-detail', args=(self.user.pk, ))
+        self.url_post = reverse('api:user-list')
+        self.url_list = reverse('api:user-list')
+
+        self.user_detail_response = {
+            'username': 'user', 'gender': 'n', 'is_active': True,
+            'avatar': '', 'nickname': 'user',
+            'date_joined': '2013-03-18T04:39:07.267'
+        }
+
+        self.patch = {
+            'username': 'patched_user',
+        }
+
+        self.put = {
+            'id': self.user.pk,
+            'username': 'new_username',
+            'nickname': 'man',
+            'gender': 'm',
+            'email': 'new_uer@blacklibrary.ru',
+        }
+        self.post = {
+            'username': 'new_username',
+            'nickname': 'man',
+            'gender': 'm',
+            'email': 'new_uer@blacklibrary.ru',
+        }
+
+    # test anonymous user
+    def test_anonymous_get_user_detail(self):
+        response = self.client.get(self.url_detail, format='json')
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response['Content-Type'], 'application/json')
+        load = json.loads(response.content)
+        self.assertEqual(load['detail'], 'Not found')
+
+    def test_anonymous_get_user_list(self):
+        response = self.client.get(self.url_list, format='json')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response['Content-Type'], 'application/json')
+        load = json.loads(response.content)
+        self.assertEqual(len(load['results']), 0)
+
+    def test_anonymous_put_user_detail(self):
+        response = self.client.put(self.url_put, data=self.put,
+                                   format='json')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(response['Content-Type'], 'application/json')
+        load = json.loads(response.content)
+
+        self.assertEqual(load['put'], 'permission denied')
+
+    def test_anonymous_post_user_list(self):
+        response = self.client.post(self.url_post, data=self.post,
+                                    format='json')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(response['Content-Type'], 'application/json')
+        load = json.loads(response.content)
+        self.assertEqual(load['post'], 'permission denied')
+
+    def test_anonymous_patch_user_detail(self):
+        response = self.client.patch(self.url_patch, data=self.patch,
+                                     format='json')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(response['Content-Type'], 'application/json')
+        load = json.loads(response.content)
+        self.assertEqual(load['patch'], 'permission denied')
+
+    def test_anonymous_delete_user_detail(self):
+        response = self.client.delete(self.url_delete, data={},
+                                      format='json')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(response['Content-Type'], 'application/json')
+        load = json.loads(response.content)
+        self.assertEqual(load['delete'], 'permission denied')
+
+    # test admin user
+    def test_admin_get_user_detail(self):
+        self.login('admin')
+        response = self.client.get(self.url_detail, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response['Content-Type'], 'application/json')
+        load = json.loads(response.content)
+        self.assertEqual(load, self.user_detail_response)
+
+    def test_admin_get_user_list(self):
+        self.login('admin')
+        response = self.client.get(self.url_list, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response['Content-Type'], 'application/json')
+        load = json.loads(response.content)
+        self.assertEqual(len(load['results']), User.objects.count())
+
+    def test_admin_put_user_detail(self):
+        self.login('admin')
+        count = User.objects.count()
+        response = self.client.put(self.url_put, data=self.put,
+                                   format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response['Content-Type'], 'application/json')
+        load = json.loads(response.content)
+        put = deepcopy(self.put)
+        # non serialized fields
+        put.pop('id')
+        put.pop('email')
+
+        for field, item in put.items():
+            self.assertEqual(load[field], put[field])
+        self.assertEqual(User.objects.count(), count)
+
+    def test_admin_post_user_list(self):
+        self.login('admin')
+        count = User.objects.count()
+        response = self.client.post(self.url_post, data=self.post,
+                                    format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response['Content-Type'], 'application/json')
+
+        load = json.loads(response.content)
+        post = deepcopy(self.post)
+        post.pop('email')
+        self.assertEqual(User.objects.count(), count + 1)
+        for field, value in post.items():
+            self.assertEqual(load[field], post[field])
+
+    def test_admin_patch_user_detail(self):
+        self.login('admin')
+        count = User.objects.count()
+        response = self.client.patch(self.url_patch, data=self.patch,
+                                     format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response['Content-Type'], 'application/json')
+        load = json.loads(response.content)
+        user = User.objects.get(pk=self.user.pk)
+        for field, value in self.patch.items():
+            self.assertEqual(getattr(user, field), self.patch[field])
+            self.assertEqual(load[field], self.patch[field])
+        self.assertEqual(User.objects.count(), count)
+
+    def test_admin_delete_user_detail(self):
+        self.login('admin')
+        count = User.objects.count()
+        response = self.client.delete(self.url_delete, data={},
+                                      format='json')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(User.objects.count(), count - 1)
--- apps/accounts/views/rest.py
+++ apps/accounts/views/rest.py
@@ -2,6 +2,8 @@
 import django_filters
 from apps.accounts.models import User
 from rest_framework import viewsets
+from rest_framework import status
+from rest_framework.response import Response
 from apps.accounts.serializers import UserSerializer
 
 
@@ -10,7 +12,49 @@ class UserFilterSet(django_filters.FilterSet):
         model = User
 
 
-class UserViewSet(viewsets.ModelViewSet):
+class UserCRUDAccessMixin(object):
+    def create(self, request, *args, **kwargs):
+        if (request.user.is_authenticated()
+                and request.user.has_perm('accounts.change_user')):
+            return super(UserCRUDAccessMixin, self).create(request, *args,
+                                                           **kwargs)
+
+        serializer = super(UserCRUDAccessMixin, self).get_serializer(
+            data=request.DATA, files=request.FILES)
+        errors = {
+            request.method.lower(): 'permission denied'
+        }
+        errors.update(serializer.errors)
+        return Response(errors, status=status.HTTP_403_FORBIDDEN)
+
+    def update(self, request, *args, **kwargs):
+        if (request.user.is_authenticated()
+                and request.user.has_perm('accounts.change_user')):
+            return super(UserCRUDAccessMixin, self).update(request, *args,
+                                                           **kwargs)
+        serializer = super(UserCRUDAccessMixin, self).get_serializer(
+            data=request.DATA, files=request.FILES)
+        errors = {
+            request.method.lower(): 'permission denied'
+        }
+        errors.update(serializer.errors)
+        return Response(errors, status=status.HTTP_403_FORBIDDEN)
+
+    def destroy(self, request, *args, **kwargs):
+        if (request.user.is_authenticated()
+                and request.user.has_perm('accounts.change_user')):
+            return super(UserCRUDAccessMixin, self).destroy(request, *args,
+                                                            **kwargs)
+        serializer = super(UserCRUDAccessMixin, self).get_serializer(
+            data=request.DATA, files=request.FILES)
+        errors = {
+            request.method.lower(): 'permission denied'
+        }
+        errors.update(serializer.errors)
+        return Response(errors, status=status.HTTP_403_FORBIDDEN)
+
+
+class UserViewSet(UserCRUDAccessMixin, viewsets.ModelViewSet):
     """
     API viewpoint for event
     """
@@ -18,8 +62,13 @@ class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     # filter_class = UserSerializer
 
+
     def get_queryset(self):
         qs = super(UserViewSet, self).get_queryset()
         if self.request.user.is_authenticated():
-            return qs.filter(pk=self.request.user.pk)
+
+            return (
+                qs if self.request.user.has_perm('accounts.change_user')
+                else qs.filter(pk=self.request.user.pk)
+            )
         return qs.none()
