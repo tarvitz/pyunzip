855d13a4c491ffa982e4acd929156e9dd6f65198
Tarvitz <tarvitz@blacklibrary.ru>
2014-07-02 11:26:10 +0400

    update core drf permissions

--- apps/core/api.py
+++ apps/core/api.py
@@ -3,12 +3,34 @@ from django.core.exceptions import ImproperlyConfigured
 from rest_framework import permissions
 
 
-class IsOwnerOrModelAdminOrReadOnly(permissions.BasePermission):
+class AdminModelPermissionMixin(object):
+    _permission_format = '%(app)s.%(prefix)s_%(model)s'
+
+    def get_admin_model_permission(self, request, view):
+        if request.method == 'POST':
+            prefix = 'add'
+        elif request.method in ('PUT', 'PATCH'):
+            prefix = 'change'
+        elif request.method == 'DELETE':
+            prefix = 'delete'
+        else:
+            prefix = 'change'
+        app = view.queryset.model._meta.app_label
+        model = view.queryset.model._meta.model_name
+        perm = self._permission_format % {
+            'app': app,
+            'model': model,
+            'prefix': prefix
+        }
+        return perm
+
+
+class IsOwnerOrModelAdminOrReadOnly(AdminModelPermissionMixin,
+                                    permissions.BasePermission):
     """
     Object-level permission to allow admins to object modifications.
     Allow access object owners to instances.
     """
-    _permission_format = '%(app)s.%(prefix)s_%(model)s'
     owner_fields = ['user', 'owner']
 
     def get_owner_fields(self):
@@ -31,23 +53,7 @@ class IsOwnerOrModelAdminOrReadOnly(permissions.BasePermission):
         if is_owner:
             return is_owner
 
-        if request.method == 'POST':
-            prefix = 'add'
-        elif request.method in ('PUT', 'PATCH'):
-            prefix = 'change'
-        elif request.method == 'DELETE':
-            prefix = 'delete'
-        else:
-            prefix = 'change'
-
-        app = obj._meta.app_label
-        model_name = obj._meta.model_name
-
-        perm = self._permission_format % {
-            'app': app,
-            'model': model_name,
-            'prefix': prefix
-        }
+        perm = self.get_admin_model_permission(request, view)
         return request.user and request.user.has_perm(perm)
 
 
@@ -75,7 +81,8 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
             )
 
 
-class IsModelAdminOrReadOnly(permissions.BasePermission):
+class IsModelAdminOrReadOnly(AdminModelPermissionMixin,
+                             permissions.BasePermission):
     """
     Object-level permission to allow admins to object modifications.
     """
@@ -84,34 +91,15 @@ class IsModelAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.method in permissions.SAFE_METHODS:
             return True
-        model = view.queryset.model
-        app = model._meta.app_label
-        model_name = model._meta.model_name
-        perm = self._permission_format % {
-            'app': app,
-            'model': model_name,
-            'prefix': 'add'
-        }
+        perm = self.get_admin_model_permission(request, view)
         return request.user and request.user.has_perm(perm)
 
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
-        if request.method == 'POST':
-            prefix = 'add'
-        elif request.method in ('PUT', 'PATCH'):
-            prefix = 'change'
-        elif request.method == 'DELETE':
-            prefix = 'delete'
-        else:
-            prefix = 'change'
-
-        app = obj._meta.model.app_label
-        model_name = obj._meta.model.model_name
-
-        perm = self._permission_format % {
-            'app': app,
-            'model': model_name,
-            'prefix': prefix
-        }
+        perm = self.get_admin_model_permission(request, view)
         return request.user and request.user.has_perm(perm)
+
+
+class IsOwnerOrModelAdminOrPermissionDenied(permissions.BasePermission):
+    pass
