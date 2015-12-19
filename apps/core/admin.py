1b05a2ee934e74f2e9789b1d2d79da3a82ba94ba
Tarvitz <tarvitz@blacklibrary.ru>
2015-12-19 23:25:56 +0300

    [locale] update admin headers

--- apps/core/admin.py
+++ apps/core/admin.py
@@ -1,6 +1,9 @@
 # coding: utf-8
 from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
 from . import models
 
 
 admin.site.register(models.UserSID)
+admin.site.site_header = _("Tessera admin")
+admin.site.site_title = _("Tessera")
